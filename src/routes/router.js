a2936015b3271ca8a68d3fdd28ffc0ffe490a3e8
higiniecito <cdramirezh@unal.edu.co>
2024-05-21 14:32:19 -0500

    Add /api/v1 to router

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,7 +1,14 @@
+import e from "express";
 import { usersRouter } from "./user.router.js";
 import { labelsRouter } from "./label.router.js";
 
+// Setting up router v1
+const routerV1 = e.Router();
+
+routerV1.use("/users", usersRouter);
+routerV1.use("/labels", labelsRouter);
+// End setting up router v1
+
 export const setUpRouter = (app) => {
-	app.use("/users", usersRouter);
-	app.use("/labels", labelsRouter);
+	app.use("/api/v1", routerV1);
 };
