05409e870258a4cb27acd8f9ee6ff5353fdf5a01
higiniecito <cdramirezh@unal.edu.co>
2024-05-21 15:19:43 -0500

    write basic crud for users

--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -9,4 +9,36 @@ router.get("/", async (req, res) => {
 	res.send(usuarios);
 });
 
+router.get("/:id", async (req, res) => {
+	const { id } = req.params;
+	const user = await service.findOne(id);
+	res.json(user);
+});
+
+router.post("/", async (req, res) => {
+	const body = req.body;
+	const user = await service.create(body);
+	res.json(user);
+});
+
+router.put("/:id", async (req, res) => {
+	const { id } = req.params;
+	const body = req.body;
+	const user = await service.update(id, body);
+	res.json(user);
+});
+
+router.patch("/:id", async (req, res) => {
+	const { id } = req.params;
+	const body = req.body;
+	const user = await service.update(id, body);
+	res.json(user);
+});
+
+router.delete("/:id", async (req, res) => {
+	const { id } = req.params;
+	await service.delete(id);
+	res.status(201).json({ id });
+});
+
 export const usersRouter = router;
--- src/services/user.service.js
+++ src/services/user.service.js
@@ -9,4 +9,25 @@ export class UserService {
 		const users = await User.findAll({ include: "labels" });
 		return users;
 	}
+
+	async findOne(id) {
+		const user = await User.findByPk(id);
+		return user;
+	}
+
+	async create(user) {
+		const newUser = await User.create(user);
+		return newUser;
+	}
+
+	async update(id, changes) {
+		const user = await this.findOne(id);
+		return await user.update(changes);
+	}
+
+	async delete(id) {
+		const user = await this.findOne(id);
+		await user.destroy();
+		return { id };
+	}
 }
